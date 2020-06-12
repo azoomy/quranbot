7945eb240d75d30337679b884397e302fd3dc61a
azoomy <56090397+azoomy@users.noreply.github.com>
2020-06-12 09:52:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from random import choice
 from verses import Verses
-import logging
 from time import sleep
 from config import create_api
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
+
 api = create_api()
 
 
 def tweet():
     while True:
         verses = choice(Verses)
-        logger.info("updating status.....")
         api.update_status(verses)
-        logger.info("sleeping")
-        sleep(60*60*8)
+        sleep(60*60*12)
 
 
 if __name__ == "__main__":
     tweet()
+    
