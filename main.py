3c642d70766586fdfafb848e0f4b791e5dd3b4d4
Azeem <56090397+azoomy@users.noreply.github.com>
2021-11-26 03:30:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from random import choice
 from verses import Verses
+import logging
+from hadiths import Hadiths
 from time import sleep
 from config import create_api
 
-
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 api = create_api()
 
 
 def tweet():
     while True:
+        hadith = choice(Hadiths)
         verses = choice(Verses)
+        logger.info("updating status.....")
         api.update_status(verses)
-        sleep(60*60*12)
+        api.update_status(hadith)
+        logger.info("sleeping")
+        sleep(60 * 60 * 12)
 
 
 if __name__ == "__main__":
     tweet()
-    
