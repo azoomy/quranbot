9cf0df29c9c97a683edb6c53b82841eaa4aa338b
Abdul Azeem <m.abdul.azeem@kmzee.online>
2022-10-20 23:42:56 +0530

    feat: dynamically tweeting verses through API

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
 import tweepy
 import logging
 import os
-import credentials
-import time
+from dotenv import load_dotenv
+load_dotenv()
 
 logger = logging.getLogger()
 
 
 def create_api():
-    consumer_key = os.getenv(credentials.CONSUMER_KEY)
-    consumer_secret = os.getenv(credentials.CONSUMER_SECRET)
-    access_token = os.getenv(credentials.ACCESS_TOKEN)
-    access_token_secret = os.getenv(credentials.ACCESS_TOKEN_SECRET)
-    auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
-    auth.set_access_token(credentials.ACCESS_TOKEN, credentials.ACCESS_TOKEN_SECRET)
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
+
     try:
         api.verify_credentials()
     except Exception as e:
--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-from random import choice
-from verses import Verses
 import logging
-from hadiths import Hadiths
-from time import sleep
 from config import create_api
+from random import randint
+from time import sleep
+import requests
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 api = create_api()
 
 
+def bring_verse(verse):
+    url = 'http://api.alquran.cloud/ayah/' + str(verse) + '/editions/quran-uthmani,en.pickthall'
+    json_data = requests.get(url).json()
+    verse_a = json_data['data'][0]['text']
+    verse_en = json_data['data'][1]['text']
+    surah = json_data['data'][0]['surah']['englishName'] + \
+            '(' + str(json_data['data'][0]['surah']['number']) + '):' + \
+            str(json_data['data'][0]['numberInSurah'])
+    return [verse_en, surah]
+
+
+def main():
+    ayah_no = randint(1, 6237)
+    ayah = bring_verse(ayah_no)
+    if len(ayah[0]) > 250:
+        main()
+    else:
+        ayah_tweet = ayah[0] + " - " + ayah[1]
+        logger.info(ayah_tweet)
+        api.update_status(ayah_tweet)
+
+
 def tweet():
     while True:
-        hadith = choice(Hadiths)
-        verses = choice(Verses)
-        logger.info("updating status.....")
-        api.update_status(verses)
-        api.update_status(hadith)
-        logger.info("sleeping")
-        sleep(60 * 60 * 12)
+        logger.info("updating...")
+        main()
+        logger.info("sleeping...")
+        sleep(60 * 60 * 4)
 
 
 if __name__ == "__main__":
